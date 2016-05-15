27a76bb4ccce4af4364b1162575ff9f290b37be3
Ori Menashe <orim@sedonasys.com>
2016-05-15 19:40:22 +0300

    version change

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pytest-scenario',
-      version='1.0',
+      version='1.0a1',
       description='pytest plugin for test scenarios',
       author='Ori Menashe',
       author_email='orim@live.com',
